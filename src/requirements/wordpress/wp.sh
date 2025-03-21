ab19a17c9e95f26385cc47483cb525e921a6be04
Timur Schmidt <timur.schmidt.ts@gmail.com>
2025-03-21 16:17:13 +0100

    finally got the wp-config to copy over

--- src/requirements/wordpress/wp.sh
+++ src/requirements/wordpress/wp.sh
@@ -5,4 +5,8 @@ curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.pha
 chmod +x wp-cli.phar
 ./wp-cli.phar core download --allow-root
 
+cd /
+cp wp-config.php /var/www/html/
+cd -
+
 php-fpm7.4 -F
