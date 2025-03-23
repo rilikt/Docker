2ace07b23a922d74f9e1e028da8e43967ff9c6ee
h4ns <timur.schmidt.ts@gmail.com>
2025-03-23 21:27:23 +0100

    finally got it back up running again, found a very useful docker command and added a makefile command for it. as well as doing some smaller tweaks on the containers

--- src/requirements/mariadb/db.sh
+++ src/requirements/mariadb/db.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-set -e
-set -x
+# set -e
+# set -x
 
-service mariadb start
+cp /50-server.cnf /etc/mysql/mariadb.conf.d/
 
-# mkdir -p var/run/mysql
-# chown -R mysql:mysql var/run/mysql
-# chmod -R 755 var/run/mysql
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Installing mariadb"
+    mysql_install_db --user=mysql --ldata=/var/lib/mysql
+    chown -R mysql:mysql /var/lib/mysql
+    chmod -R 755 /var/lib/mysql
+fi
 
-# mysql_install_db
+echo "starting mariadb"
+mariadbd &
 
+while ! mysqladmin ping -h localhost --silent; do
+    sleep 3
+done
+echo "mariadb running, configuring next"
+
+#this is redundant and can be removed
 MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
 MYSQL_DATABASE=${MYSQL_DATABASE}
 MYSQL_USER=${MYSQL_USER}
 MYSQL_PASSWORD=${MYSQL_PASSWORD}
 
-# @ % means from any host, might not be needed
 mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
 CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 CREATE USER IF NOT EXISTS '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';
@@ -24,6 +33,6 @@ GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%';
 FLUSH PRIVILEGES;
 EOSQL
 
-service mariadb stop
+echo "done"
 
-exec mysqld #--socket=/var/run/mysqld/mysqld.sock
+wait
--- src/requirements/wordpress/wp.sh
+++ src/requirements/wordpress/wp.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
-# rm -f /usr/local/bin/wp
-cd /var/www/html
-
 # set -e
 # set -x
 
-# sleep 10
+cd /var/www/html
 
-# until nc -z mariadb 3306; do
-# 	sleep 3
-# done
+echo "Waiting for mariadb to be ready..."
+until nc -z mariadb 3306; do
+	sleep 3
+done
+echo "Database ready."
 
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-chmod +x wp-cli.phar
-./wp-cli.phar core download --allow-root
-# ./wp-cli.phar config create --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST} --allow-root
-# ./wp-cli.phar core install --url=localhost:8080 --title=Wordpress --admin_user=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_ADMIN_PASS} --admin_email=${WORDPRESS_ADMIN_MAIL} --allow-root
-# ./wp-cli.phar user create ${WORDPRESS_USER} ${WORDPRESS_USER_MAIL} --role=author --user_password=${WORDPRESS_USER_PASS}
 
-cp /wp-config.php .
+if [ -z "$(ls -A /var/www/html/)" ]; then
+	echo "Installing and configuring wordpress."
+	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+	chmod +x wp-cli.phar
+	./wp-cli.phar core download --allow-root
+	./wp-cli.phar config create --dbname=${WORDPRESS_DB_NAME} --dbuser=${WORDPRESS_DB_USER} --dbpass=${WORDPRESS_DB_PASSWORD} --dbhost=${WORDPRESS_DB_HOST} --allow-root
+	./wp-cli.phar core install --url=localhost --title=Wordpress --admin_user=${WORDPRESS_ADMIN} --admin_password=${WORDPRESS_ADMIN_PASS} --admin_email=${WORDPRESS_ADMIN_MAIL} --allow-root
+	# ./wp-cli.phar user create ${WORDPRESS_USER} ${WORDPRESS_USER_MAIL} --role=author --user_password=${WORDPRESS_USER_PASS}
+	echo "Done"
+fi
 
+# cp /wp-config.php .
+
+echo "Starting wordpress"
 php-fpm7.4 -F
